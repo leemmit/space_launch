1c15362ba55a6e38af57aaa1c4f6e78a746338b1
leemmit <kolmykovl226@gmail.com>
2024-10-30 00:00:42 +0300

    code formattor prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import IMAGE from '../public/pngwing.com.png'
 import SVG from '../public/react.svg'
 
 export const App = () => {
-    const name = 'leo'
-    return (
+  const name = 'leo'
+  return (
     <>
-        <h1>RTW Starter</h1>
-        <img src={IMAGE} alt="React Logo" />
-        <img src={SVG} alt="React Logo" />
+      <h1>RTW Starter</h1>
+      <img src={IMAGE} alt="React Logo" />
+      <img src={SVG} alt="React Logo" />
     </>
-    )
+  )
 }
