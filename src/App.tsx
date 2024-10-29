bc166a0b4770886c7dbc31d4835a27a7a8d72556
leemmit <kolmykovl226@gmail.com>
2024-10-29 22:26:16 +0300

    configuring webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import IMAGE from '../public/pngwing.com.png'
+import SVG from '../public/react.svg'
+
 export const App = () => {
-    return <h1>RTW Starter</h1>
+    return (
+    <>
+        <h1>RTW Starter</h1>
+        <img src={IMAGE} alt="React Logo" />
+        <img src={SVG} alt="React Logo" />
+    </>
+    )
 }
