dd8261d25d2a3d1b2226850c570e5fc10aff80d5
leemmit <kolmykovl226@gmail.com>
2024-10-29 23:51:17 +0300

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import IMAGE from '../public/pngwing.com.png'
 import SVG from '../public/react.svg'
 
 export const App = () => {
+    const name = 'leo'
     return (
     <>
         <h1>RTW Starter</h1>
