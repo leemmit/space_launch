65a1a2809c2b445b0946651b9e0d054dbc25a12a
leemmit <kolmykovl226@gmail.com>
2024-10-29 23:01:17 +0300

    added react-refresh-webpack-plugin

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,4 +1,14 @@
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const { plugins } = require('./webpack.common');
+
 module.exports = {
     mode: 'development',
+    devServer: {
+        hot: true,
+        open: true,
+    },
     devtool: 'cheap-module-source-map',
+    plugins: [
+        new ReactRefreshWebpackPlugin(),
+    ]
 }
